4a8eb11261219cb52ea7b3458f6cc4b027c699f5
Nidhin <nidhinpd811@gmail.com>
2026-02-21 01:20:16 +0530

    FInetune test skipped

--- packages/qvac-lib-infer-llamacpp-llm/test/mobile/integration.auto.cjs
+++ packages/qvac-lib-infer-llamacpp-llm/test/mobile/integration.auto.cjs
@@ -7,60 +7,50 @@ require('./integration-runtime.cjs')
 /* global runIntegrationModule */
 
 async function runApiBehaviorTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/api-behavior.test.js', options)
 }
 
 async function runCacheStateMachineTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/cache-state-machine.test.js', options)
 }
 
 async function runConfigParametersTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/config-parameters.test.js', options)
 }
 
 async function runFinetuningPauseResumeTest (options = {}) { // eslint-disable-line no-unused-vars
+  return
   return runIntegrationModule('../integration/finetuning-pause-resume.test.js', options)
 }
 
 async function runImageTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/image.test.js', options)
 }
 
 async function runModelLoadingTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/model-loading.test.js', options)
 }
 
 async function runMoeTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/moe.test.js', options)
 }
 
 async function runMultiInstanceTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/multi-instance.test.js', options)
 }
 
 async function runReasoningTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/reasoning.test.js', options)
 }
 
 async function runSlidingContextTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/sliding-context.test.js', options)
 }
 
 async function runToolCallingTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/tool-calling.test.js', options)
 }
 
 async function runUtf8OutputTest (options = {}) { // eslint-disable-line no-unused-vars
-  return
   return runIntegrationModule('../integration/utf8-output.test.js', options)
 }
