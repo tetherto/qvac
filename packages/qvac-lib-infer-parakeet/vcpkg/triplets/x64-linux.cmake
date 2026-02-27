3c60b578f267382249964bdd6c4eccf850e237ab
Juan Pablo Garibotti Arias <juan.arias@bitfinex.com>
2026-02-27 19:22:30 +0100

    Add -Wno-array-bounds to parakeet Linux x64 triplet
    Suppress false positive array bounds warning in onnxruntime MLAS
    AVX2/AVX512 code when building with clang-19.

--- packages/qvac-lib-infer-parakeet/vcpkg/triplets/x64-linux.cmake
+++ packages/qvac-lib-infer-parakeet/vcpkg/triplets/x64-linux.cmake
@@ -5,5 +5,5 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/linux-clang.cmake")
 set(VCPKG_C_FLAGS "-fPIC")
-set(VCPKG_CXX_FLAGS "-fPIC -stdlib=libc++")
+set(VCPKG_CXX_FLAGS "-fPIC -stdlib=libc++ -Wno-array-bounds")
 set(VCPKG_LINKER_FLAGS "-stdlib=libc++")
