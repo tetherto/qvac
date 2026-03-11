e527209dc6b7617eab6e02ce900ef0b622fe4d7a
Bruno Campana <7632562+BrunoCampana@users.noreply.github.com>
2026-03-11 07:42:01 -0300

    QVAC-13983 doc: reorganize monorepo docs structure (#785)
    * doc[skiplog]: reorganize monorepo documentation structure - update README
    
    * doc[skiplog]: reorganize monorepo documentation structure - move architecture docs to /docs
    
    * doc[skiplog]: reorganize monorepo documentation structure - remove docs website from packages
    
    * doc[skiplog]: reorganize monorepo documentation structure - move gitflow to /docs
    
    * doc[skiplog]: reorganize monorepo documentation structure - remove docs website from sdk package

--- packages/docs/src/app/api/search/route.ts
+++
@@ -1,6 +0,0 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
-
-// statically cached
-export const revalidate = false;
-export const { staticGET: GET } = createFromSource(source);
