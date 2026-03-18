429c029abacf46baa3de29321bbda05fc4a82020
Lucas Tortora <lucas.tortora@tether.io>
2026-03-18 14:38:08 -0300

    update icon

--- docs/website/src/lib/trees/latest.ts
+++ docs/website/src/lib/trees/latest.ts
@@ -47,18 +47,6 @@ export const tree: Node[] = [
         icon: resolveIcon('Globe'),
       },
       {
-        name: 'Public launch',
-        url: '/about-qvac/public-launch',
-        type: 'page',
-        icon: resolveIcon('Globe'),
-      },
-      {
-        name: 'Public launch',
-        url: '/public-launch',
-        type: 'page',
-        icon: resolveIcon('Globe'),
-      },
-      {
         name: 'How it works',
         url: '/about-qvac/how-it-works',
         type: 'page',
