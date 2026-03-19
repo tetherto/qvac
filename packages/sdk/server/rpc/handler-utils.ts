6e26d627687f37e4b0cbb183aa6a5df02a2a12bb
opaninakuffo <opaninakuffo@outlook.com>
2026-03-19 17:30:52 +0000

    refactor: server metrics in done chunk for streams

--- packages/sdk/server/rpc/handler-utils.ts
+++ packages/sdk/server/rpc/handler-utils.ts
@@ -15,6 +15,15 @@ import { setSDKConfig } from "@/server/bare/registry/config-registry";
 import { setRuntimeContext } from "@/server/bare/registry/runtime-context-registry";
 import { type ServerProfiler } from "./profiling";
 
+export function isTerminalChunk(value: unknown): value is { done: true } {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    "done" in value &&
+    (value as { done: unknown }).done === true
+  );
+}
+
 function getProfilingMetaFromRequest(
   request: Request,
 ): ProfilingRequestMeta | undefined {
@@ -79,6 +88,7 @@ async function executeStreamHandler(
 ) {
   const stream = req.createResponseStream();
   profiler.startHandler();
+  let sentFinalChunk = false;
 
   try {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -92,13 +102,24 @@ async function executeStreamHandler(
     } else {
       generator = handler(request);
     }
+
     for await (const response of generator) {
-      stream.write(profiler.serialize(response, false) + "\n", "utf-8");
+      if (isTerminalChunk(response)) {
+        profiler.endHandler();
+        stream.write(profiler.serialize(response, true) + "\n", "utf-8");
+        sentFinalChunk = true;
+      } else {
+        stream.write(profiler.serialize(response, false) + "\n", "utf-8");
+      }
     }
-    profiler.endHandler();
-    const trailer = profiler.serialize();
-    if (trailer) {
-      stream.write(trailer + "\n", "utf-8");
+    
+    // Fallback
+    if (!sentFinalChunk) {
+      profiler.endHandler();
+      const trailer = profiler.serialize();
+      if (trailer) {
+        stream.write(trailer + "\n", "utf-8");
+      }
     }
 
     stream.end();
