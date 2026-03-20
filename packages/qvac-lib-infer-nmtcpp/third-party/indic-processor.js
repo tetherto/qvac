d8de87e32b92e26ab16a396f55f910a4d447556d
olyasir <sirkinolya@gmail.com>
2026-03-20 16:06:17 +0200

    fix[notask]: bound regex quantifiers to eliminate polynomial ReDoS
    Replace unbounded \d+ with \d{1,20} and \w+ with \w{1,100} in
    _NUMERAL_PATTERN and _OTHER_PATTERN to make backtracking constant-time
    regardless of input length. No real-world numeral exceeds 20 digits
    and no hashtag/mention exceeds 100 chars.

--- packages/qvac-lib-infer-nmtcpp/third-party/indic-processor.js
+++ packages/qvac-lib-infer-nmtcpp/third-party/indic-processor.js
@@ -222,9 +222,9 @@ class IndicProcessor {
     this._URL_PATTERN =
       /\b(?<![\w/.])(?:(?:https?|ftp):\/\/)?(?:[\w-]+\.)+(?!\.)[\w/\-?#&=%.]*(?!\.\w+)\b/g
     this._NUMERAL_PATTERN =
-      /(~?\d+(?:\.\d+)?\s?%?\s?-?\s?~?\d+(?:\.\d+)?\s?%|~?\d+%|\d+[-/.,:']\d+[-/.,:']+\d+(?:\.\d+)?|\d+[-/.:'+]\d+(?:\.\d+)?)/g
+      /(~?\d{1,20}(?:\.\d{1,20})?\s?%?\s?-?\s?~?\d{1,20}(?:\.\d{1,20})?\s?%|~?\d{1,20}%|\d{1,20}[-/.,:']\d{1,20}[-/.,:']{1,5}\d{1,20}(?:\.\d{1,20})?|\d{1,20}[-/.:'+]\d{1,20}(?:\.\d{1,20})?)/g
     this._EMAIL_PATTERN = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}/g
-    this._OTHER_PATTERN = /[A-Za-z0-9]{0,100}[#|@]\w+/g
+    this._OTHER_PATTERN = /[A-Za-z0-9]{0,100}[#|@]\w{1,100}/g
 
     // Combined punctuation replacements
     this._PUNC_REPLACEMENTS = [
