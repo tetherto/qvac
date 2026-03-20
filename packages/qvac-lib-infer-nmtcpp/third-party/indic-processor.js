dd3d7263d3def827496b0e4fe883a093947e6d0c
olyasir <sirkinolya@gmail.com>
2026-03-20 14:39:28 +0200

    fix[notask]: resolve code scanning security findings in nmtcpp and ocr-onnx
    Fix ReDoS vulnerabilities in indic-processor URL and numeral regexes by
    removing nested quantifiers. Fix ReDoS in sacremoses tokenizer protected
    patterns by requiring opening quotes to eliminate ambiguous backtracking.
    Fix incomplete string replacement in indic_normalize by using global
    regex for pipe character substitution. Replace insecure tempfile.mktemp
    with NamedTemporaryFile in ocr-onnx benchmark script.

--- packages/qvac-lib-infer-nmtcpp/third-party/indic-processor.js
+++ packages/qvac-lib-infer-nmtcpp/third-party/indic-processor.js
@@ -220,9 +220,9 @@ class IndicProcessor {
     this._END_BRACKET_SPACE_PUNC_REGEX = /\) ([.!:?;,])/g
 
     this._URL_PATTERN =
-      /\b(?<![\w/.])(?:(?:https?|ftp):\/\/)?(?:(?:[\w-]+\.)+(?!\.))(?:[\w/\-?#&=%.]+)+(?!\.\w+)\b/g
+      /\b(?<![\w/.])(?:(?:https?|ftp):\/\/)?(?:[\w-]+\.)+(?!\.)[\w/\-?#&=%.]*(?!\.\w+)\b/g
     this._NUMERAL_PATTERN =
-      /(~?\d+\.?\d*\s?%?\s?-?\s?~?\d+\.?\d*\s?%|~?\d+%|\d+[-/.,:']\d+[-/.,:'+]\d+(?:\.\d+)?|\d+[-/.:'+]\d+(?:\.\d+)?)/g
+      /(~?\d+\.?\d*\s?%?\s?-?\s?~?\d+\.?\d*\s?%|~?\d+%|\d+[-/.,:']\d+[-/.,:']+\d+(?:\.\d+)?|\d+[-/.:'+]\d+(?:\.\d+)?)/g
     this._EMAIL_PATTERN = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}/g
     this._OTHER_PATTERN = /[A-Za-z0-9]*[#|@]\w+/g
 
