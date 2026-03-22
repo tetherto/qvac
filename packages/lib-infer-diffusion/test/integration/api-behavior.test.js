401839cd212328c92111a27aefa30b57f5f51508
Maksim Smatrou <maxim.smotrov@itrexgroup.com>
2026-03-22 11:02:50 +0100

    Force cpu to investigate the black images issue

--- packages/lib-infer-diffusion/test/integration/api-behavior.test.js
+++ packages/lib-infer-diffusion/test/integration/api-behavior.test.js
@@ -19,7 +19,7 @@ const isDarwinX64 = os.platform() === 'darwin' && os.arch() === 'x64'
 const isLinuxArm64 = os.platform() === 'linux' && os.arch() === 'arm64'
 const isAndroid = os.platform() === 'android'
 const noGpu = proc.env && proc.env.NO_GPU === 'true'
-const useCpu = isDarwinX64 || isLinuxArm64 || noGpu
+const useCpu = true
 const logger = createIntegrationLogger()
 
 // Smallest model for fast behavior tests
