76c3d7b8d7390b5bcf5b9026941d948d9b8251b6
Ishan Vohra <ishanvohra@Ishans-MacBook-Air.local>
2026-04-16 17:44:43 +0530

    add integration tests

--- packages/qvac-lib-infer-onnx-tts/examples/supertonic-streaming-tts.js
+++ packages/qvac-lib-infer-onnx-tts/examples/supertonic-streaming-tts.js
@@ -13,7 +13,6 @@ const path = require('bare-path')
 const ONNXTTS = require('../')
 const { setLogger, releaseLogger } = require('../addonLogging')
 const { canPlayPcmChunks, playInt16Chunk, createChunkQueue } = require('./pcm-chunk-player')
-const { ensureSupertonicModels } = require('../test/utils/downloadModel')
 
 const SUPERTONIC_SAMPLE_RATE = 44100
 
@@ -40,8 +39,6 @@ if (!fs.existsSync(modelDir)) {
 }
 
 async function main () {
-  await ensureSupertonicModels()
-
   setLogger((priority, message) => {
     if (priority > 1) return
     const priorityNames = {
