0647a20f54aa330cd21ff7161fea09cf0299fc82
Bruno Campana <7632562+BrunoCampana@users.noreply.github.com>
2026-04-16 09:39:12 -0300

    doc: api v0.9 manually written

--- docs/website/src/lib/versions.ts
+++ docs/website/src/lib/versions.ts
@@ -8,6 +8,7 @@ export interface Version {
 export const VERSIONS: Version[] = [
   { label: 'dev', value: 'dev', isDev: true },
   { label: 'latest (v0.8.0)', value: 'v0.8.0', isLatest: true },
+  { label: 'v0.9.0', value: 'v0.9.0' },
   { label: 'v0.7.0', value: 'v0.7.0' },
 ];
 
