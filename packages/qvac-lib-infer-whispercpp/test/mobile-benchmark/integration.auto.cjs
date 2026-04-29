69593f6d49f10da535f315ddbd22efecac87555f
ogad-tether <omar.gad@tether.io>
2026-04-29 11:55:39 +0100

    Revert "fix: add benchmark-only Whisper mobile validation"
    This reverts commit 3852e2a3c7e696c5cbd9acbe2915fb491dbd218a.

--- packages/qvac-lib-infer-whispercpp/test/mobile-benchmark/integration.auto.cjs
+++
@@ -1,8 +0,0 @@
-'use strict'
-require('../mobile/integration-runtime.cjs')
-
-/* global runIntegrationModule */
-
-async function runRtfBenchmarkTest (options = {}) { // eslint-disable-line no-unused-vars
-  return runIntegrationModule('../integration/mobile-rtf-benchmark.test.js', options)
-}
