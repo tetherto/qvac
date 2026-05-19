b227c1bf8f94366abcd82500433718ba2d47a151
Marcus Edel <marcus.edel@collabora.com>
2026-05-19 01:32:12 +0000

    Point to the latest fabric changes.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- packages/embed-llamacpp/vcpkg/ports/qvac-fabric/portfile.cmake
+++ packages/embed-llamacpp/vcpkg/ports/qvac-fabric/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO tetherto/qvac-fabric-llm.cpp
-  REF 3c9c019a99275a4fcd1a7cc31cf0f270edd10ebf
-  SHA512 a130a539ff5874827a61b790f38226491914b75730d66e78bbac80268645842d40baabb2735e13eaabd52a5b80786c6b8992e5af437ca60ad7e212ada3ac49c6
+  REF b3cc11f6cef82f2f14aedc64084bae66601d9dd5
+  SHA512 80f8fb0f9007b9db072fb61fd4c4aeec5cdac3ba37d9f85096332fc7c9a325d5cc80acfdd5fdffcd974c2ff91240dda306bb63333921b1c8f70ea6608549af38
 )
 
 vcpkg_check_features(
