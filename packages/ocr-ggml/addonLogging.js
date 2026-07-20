e836de5c6e15d382ef9bb3c36588d3158996a073
Ramaz Tskhadadze <bubu@Ramazs-MacBook-Pro-2.local>
2026-07-20 10:56:33 +0400

    chore: migrate ocr-ggml wrapper to TypeScript

--- packages/ocr-ggml/addonLogging.js
+++ packages/ocr-ggml/addonLogging.js
@@ -1,8 +1,12 @@
-module.exports = {
-  get setLogger() {
-    return require('./binding').setLogger
-  },
-  get releaseLogger() {
-    return require('./binding').releaseLogger
-  }
-}
+"use strict";
+const addonLogging = {
+    get setLogger() {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports -- native binding is resolved lazily from package prebuilds.
+        return require("./binding").setLogger;
+    },
+    get releaseLogger() {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports -- native binding is resolved lazily from package prebuilds.
+        return require("./binding").releaseLogger;
+    },
+};
+module.exports = addonLogging;
